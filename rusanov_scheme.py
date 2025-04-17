1cf993dc6f684b85eeee29a788f9824dc68abd14
n.uryadova <n.uryadova@g.nsu.ru>
2025-04-17 13:36:07 +0700

    17.04.2025

--- rusanov_scheme.py
+++ rusanov_scheme.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-g = 10
+g = 9.81
 
 
 def periodical_u(a, x0, X):
@@ -25,23 +25,46 @@ def F_func(q, h):
     return np.array(q ** 2 / h + g * h ** 2 / 2)  # приведено к виду q^2/h+gh^2/2, чтобы не использовать u
 
 
-def rusanov_scheme(q_n, h_n, R, C):  # сама схема
-    # print(f"hn: {len(h_n)} qn: {len(q_n)} un: {len(u_n)}")
+def an_sol_problem_1(x, h1, h0, q1, q0, T):
+    global g
+    h2 = 2.54  # для перепада глубин 5_1
+    # h2 = 3.95 # для перепада глубин 10_1
+    # h2 = 6.2 # для перепада глубин 20_1
+    v0 = q0 / h0
+    c1 = np.sqrt(g * h1)
+    b = np.sqrt(g * (h0 + h2) / (2 * h2 * h0))
+    v2 = b * (h2 - h0)
+    q2 = h2 * v2
+    D = q2 / (h2 - h0)
+    c2 = np.sqrt(g * h2)
+    ve = -c1
+    vc = v2 - c2
+
+    xe = ve * (T)
+    xc = vc * (T)
+    xd = D * (T)
+    h3 = lambda x: 1 / (9 * g) * (x / T - (v2 + 2 * c2)) ** 2
+    q3 = lambda x: 1 / (9 * g) * (x / T - (v2 + 2 * c2)) ** 2 * 1 / 3 * (2 * x / T + (v2 + 2 * c2))
+    deep = np.piecewise(x, [x < xe, x >= xe, x >= xc, x >= xd], [h1, h3, h2, h0])
+    flow = np.piecewise(x, [x < xe, x >= xe, x >= xc, x >= xd], [q1, q3, q2, q0])
+
+    return deep, flow
+
+
+def rusanov_scheme_p(q_n, h_n, R, C, n):
+    # вот здесь периодические условия
+    h_n = np.concatenate([[h_n[n - 2]], [h_n[n - 1]], h_n, [h_n[1]], [h_n[2]]])
+    q_n = np.concatenate([[q_n[n - 2]], [q_n[n - 1]], q_n, [q_n[1]], [q_n[2]]])
 
     f_1 = F_func(q_n, h_n)  # задаем q^2/h+gh^2/2
 
-    # print(f"f1: {len(f_1)}")
     h_1 = (h_n[:-1] + h_n[1:]) / 2 - R * (q_n[1:] - q_n[:-1]) / 3  # n - 1
     q_1 = (q_n[:-1] + q_n[1:]) / 2 - R * (f_1[1:] - f_1[:-1]) / 3
-
-    # print(f"h1: {len(h_1)} q1: {len(q_1)}")
 
     f_2 = F_func(q_1, h_1)
 
     h_2 = h_n[1:-1] - 2 / 3 * R * (q_1[1:] - q_1[:-1])
     q_2 = q_n[1:-1] - 2 / 3 * R * (f_2[1:] - f_2[:-1])  # n-2
-
-    # print(f"h2: {len(h_2)} q2: {len(q_2)} f2: {len(f_2)}")
 
     w_h = h_n[4:] - h_n[3:-1] * 4 + h_n[2:-2] * 6 - h_n[1:-3] * 4 + h_n[:-4]
     w_q = q_n[4:] - q_n[3:-1] * 4 + q_n[2:-2] * 6 - q_n[1:-3] * 4 + q_n[:-4]
@@ -49,7 +72,29 @@ def rusanov_scheme(q_n, h_n, R, C):  # сама схема
     f_3 = F_func(q_2, h_2)
     h_3 = h_n[2:-2] - R * (7 / 24 * (q_n[3:-1] - q_n[1:-3]) - (2 / 24) * (q_n[4:] - q_n[:-4])) - 3 / 8 * R * (
             q_2[2:] - q_2[:-2]) - w_h * C / 24
-    # print(f"h3[]: {len(h_3)}")
+    q_3 = q_n[2:-2] - R * ((7 / 24) * (f_1[3:-1] - f_1[1:-3]) - (2 / 24) * (f_1[4:] - f_1[:-4])) - 3 / 8 * R * (
+            f_3[2:] - f_3[:-2]) - w_q * C / 24
+
+    return h_3, q_3
+
+
+def rusanov_scheme_s(q_n, h_n, R, C):  # сама схема
+    f_1 = F_func(q_n, h_n)  # задаем q^2/h+gh^2/2
+
+    h_1 = (h_n[:-1] + h_n[1:]) / 2 - R * (q_n[1:] - q_n[:-1]) / 3  # n - 1
+    q_1 = (q_n[:-1] + q_n[1:]) / 2 - R * (f_1[1:] - f_1[:-1]) / 3
+
+    f_2 = F_func(q_1, h_1)
+
+    h_2 = h_n[1:-1] - 2 / 3 * R * (q_1[1:] - q_1[:-1])
+    q_2 = q_n[1:-1] - 2 / 3 * R * (f_2[1:] - f_2[:-1])  # n-2
+
+    w_h = h_n[4:] - h_n[3:-1] * 4 + h_n[2:-2] * 6 - h_n[1:-3] * 4 + h_n[:-4]
+    w_q = q_n[4:] - q_n[3:-1] * 4 + q_n[2:-2] * 6 - q_n[1:-3] * 4 + q_n[:-4]
+
+    f_3 = F_func(q_2, h_2)
+    h_3 = h_n[2:-2] - R * (7 / 24 * (q_n[3:-1] - q_n[1:-3]) - (2 / 24) * (q_n[4:] - q_n[:-4])) - 3 / 8 * R * (
+            q_2[2:] - q_2[:-2]) - w_h * C / 24
     q_3 = q_n[2:-2] - R * ((7 / 24) * (f_1[3:-1] - f_1[1:-3]) - (2 / 24) * (f_1[4:] - f_1[:-4])) - 3 / 8 * R * (
             f_3[2:] - f_3[:-2]) - w_q * C / 24
 
@@ -57,6 +102,14 @@ def rusanov_scheme(q_n, h_n, R, C):  # сама схема
     q_3 = np.concatenate([[q_n[0]], [q_n[1]], q_3, [q_n[-2]], [q_n[-1]]])
 
     return h_3, q_3
+
+
+def calculate_p(u1, u2, u3):
+    u1 = u1[2:-2]
+    u2 = u2[2:-2][::2]
+    u3 = u3[2:-2][::4]
+    p = np.log(abs((u1 - u2) / (u2 - u3)))
+    return p
 
 
 def runge_error(u_h, u_h2, p):
@@ -70,13 +123,14 @@ def rusanov_scheme_for_step(C, X, x_start, x_end, T, CFL, n0, H0, H1):
 
     x0 = np.linspace(x_start, x_end,
                      n)
-    u0 = step_u(n)
+    u0 = np.zeros(n) #step_u(n)
     h0 = step_h(x0, H0, H1)
 
     q0 = h0 * u0
     h_n, q_n, u_n = h0.copy(), q0.copy(), u0.copy()
 
     max_lambda = max(abs(u0) + (g * h0) ** 0.5)  # (max(lambda1, lambda2))
+    print(f"max lambda: {max_lambda}")
     delta_t = CFL * delta_x / max_lambda
     R = delta_t / delta_x
 
@@ -86,31 +140,29 @@ def rusanov_scheme_for_step(C, X, x_start, x_end, T, CFL, n0, H0, H1):
     time_steps = 100000000
 
     while step < time_steps:
-        h_n, q_n = rusanov_scheme(q_n, h_n, R, C)
+        h_n, q_n = rusanov_scheme_s(q_n, h_n, R, C)
         u_n = q_n / h_n
         max_lambda = max(abs(u_n) + (g * h_n) ** 0.5)
         delta_t = CFL * delta_x / max_lambda
         # динамическое вычисление шага по времени
         time_steps = int(T / delta_t) + 1
         R = delta_t / delta_x
-        # print(f"CFL: {delta_t * max_lambda / delta_x}, R: {R}")
+        print(f"CFL: {delta_t * max_lambda / delta_x}, R: {R}")
         step += 1
-        # print(step, time_steps)
-        if step > 10000:
+        print(step, time_steps, delta_t)
+        if step > 500:
             break
-    # f.close()
     return x0, h_n
 
 
 def rusanov_scheme_periodical(a, b, C, X, x_start, x_end, T, CFL, n0):
     f = open("max_lambda.txt", "w")
     n = n0 + 4
-    x0 = np.linspace(x_start, x_end, n)
+    x0 = np.linspace(x_start, x_end, n0)
     u0 = periodical_u(a, x0, X)
     h0 = periodical_h(b, u0)
     q0 = h0 * u0
     h_n, q_n, u_n = h0.copy(), q0.copy(), u0.copy()
-
 
     max_lambda = max(abs(u0) + (g * h0) ** 0.5)
     delta_x = X / n
@@ -118,13 +170,8 @@ def rusanov_scheme_periodical(a, b, C, X, x_start, x_end, T, CFL, n0):
     R = delta_t / delta_x
     time_steps = int(T / delta_t) + 1
     for i in range(time_steps):
-        h_n, q_n = rusanov_scheme(q_n, h_n, R, C)
+        h_n, q_n = rusanov_scheme_p(q_n, h_n, R, C, n0)
         u_n = q_n / h_n
-        u_n[0] = u_n[n0 - 1]
-        u_n[1] = u_n[n0]
-        u_n[2] = u_n[n0 + 1]
-        u_n[n0 + 2] = u_n[3]
-        u_n[n0 + 3] = u_n[4]
         max_lambda = max(abs(u_n) + (g * h_n) ** 0.5)
         print(max_lambda, file=f)
         delta_t = CFL * delta_x / max_lambda
@@ -134,3 +181,21 @@ def rusanov_scheme_periodical(a, b, C, X, x_start, x_end, T, CFL, n0):
     return x0, h_n
 
 
+def rusanov_scheme_third_task(a, b, C, X, x_start, x_end, T, delta_t, n0):
+    f = open("max_lambda.txt", "w")
+    n = n0 + 4
+    x0 = np.linspace(x_start, x_end, n)
+    u0 = periodical_u(a, x0, X)
+    h0 = periodical_h(b, u0)
+    q0 = h0 * u0
+    h_n, q_n, u_n = h0.copy(), q0.copy(), u0.copy()
+
+    # max_lambda = max(abs(u0) + (g * h0) ** 0.5)
+    delta_x = X / n
+    # delta_t = CFL * delta_x / max_lambda
+    R = delta_t / delta_x
+    time_steps = int(T / delta_t) + 1
+    for i in range(time_steps):
+        h_n, q_n = rusanov_scheme_p(q_n, h_n, R, C, n)
+        u_n = q_n / h_n
+    return x0, h_n, q_n
--- rusanov_scheme_different_conditions.py
+++ rusanov_scheme_different_conditions.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-from rusanov_scheme import rusanov_scheme_for_step, step_h, rusanov_scheme_periodical
+from rusanov_scheme import rusanov_scheme_for_step, step_h, rusanov_scheme_periodical, rusanov_scheme_third_task, \
+    an_sol_problem_1, calculate_p
 import matplotlib.pyplot as plt
 
 a = 2
@@ -12,20 +13,24 @@ g = 10
 
 H1 = 5
 H0 = 1
-x_start = -5
+x_start = 0
 X = 10
 x_end = x_start + X
-CFL = 0.5
+CFL = 0.5 #0.45
 
 
 def first_task_graphic(x1, h1, n1, x2, h2, n2, x3, h3, n3):
     x_step = np.linspace(x_start, x_end, 1000)
+
+    exact_sol, flow = an_sol_problem_1(x_step, H1, H0,0, 0, 0.4)
+
     step_graph = step_h(x_step, H0, H1)
     plt.figure(figsize=(10, 6))
     plt.plot(x_step, step_graph, label="step", linestyle="--")
-    plt.plot(x1, h1, label=f"n = {n1}", linestyle="dotted")
-    plt.plot(x2, h2, label=f"n = {n2}", linestyle="dotted")
-    plt.plot(x3, h3, label=f"n = {n3}", linestyle="dotted")
+    plt.plot(x_step, exact_sol, label=f"exact", linestyle="-", color="black", linewidth=1)
+    plt.plot(x1, h1, label=f"n = {n1}", marker=".",  linestyle="")
+    plt.plot(x2, h2, label=f"n = {n2}", marker=".", linestyle="")
+    plt.plot(x3, h3, label=f"n = {n3}", marker=".", linestyle="")
     plt.xlabel("x")
     plt.ylabel("h(x, t)")
     plt.legend()
@@ -35,7 +40,7 @@ def first_task_graphic(x1, h1, n1, x2, h2, n2, x3, h3, n3):
 
 def second_task_graphic(x1, h1, n1):
     plt.figure(figsize=(10, 6))
-    plt.plot(x1, h1, label=f"n = {n1}", linestyle="dotted")
+    plt.plot(x1, h1, label=f"n = {n1}\nT = 2", linestyle="dotted")
     plt.xlabel("x")
     plt.ylabel("h(x, t)")
     plt.legend()
@@ -43,7 +48,7 @@ def second_task_graphic(x1, h1, n1):
     plt.show()
 
 def first_task():
-    T = 0.3
+    T = 0.5 #передать в функцию
     n1 = 50
     n2 = 100
     n3 = 200
@@ -54,26 +59,67 @@ def first_task():
     first_task_graphic(x1, h1, n1, x2, h2, n2, x3, h3, n3)
 
 def second_task():
-    T = 0.1
+    T = 0.5
     n = 2000
     x, h = rusanov_scheme_periodical(a, b, C, X, x_start, x_end, T, CFL, n)
     max_lambda = max([float(i) for i in open("max_lambda.txt", "r").read().split()])
     print(max_lambda)
-    second_task_graphic(x, h, n)
+    # second_task_graphic(x, h, n)
     return max_lambda
 
-def third_task(max_lamdba):
-    T1 = 0.5
-    T2 = 1
-    T3 = 2
-    n1 = 1000
-    n2 = 2000
-    n3 = 4000
-    delta_x1 = X / n1
+def third_task_graphics(n, t):
+    x = np.genfromtxt(f"tmp/time_{t}_n_{n}_x.csv", delimiter=',', names=True)
+    h = np.genfromtxt(f"tmp/time_{t}_n_{n}_h.csv", delimiter=',', names=True)
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, h, label=f"n = {n}\nT = {t}", linestyle="dotted")
+    plt.xlabel("x")
+    plt.ylabel("h(x, t)")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+def third_task(max_lambda):
+    T = [0.5, 1, 2]
+    n = [1000, 2000, 4000]
+    delta_x1 = X / n[0]
     delta_t1 = CFL * delta_x1 / max_lambda
     delta_t2 = delta_t1 / 2
     delta_t3 = delta_t1 / 4
+    x1, h1, q1 = rusanov_scheme_third_task(a, b, C, X, x_start, x_end, T[0], delta_t1, n[0])
+    x2, h2, q2 = rusanov_scheme_third_task(a, b, C, X, x_start, x_end, T[0], delta_t2, n[1])
+    x3, h3, q3 = rusanov_scheme_third_task(a, b, C, X, x_start, x_end, T[0], delta_t3, n[2])
+
+    # u1 = q1/h1
+    # u2 = q2/h2
+    # u3 = q3/h3
+
+    pq = calculate_p(q1, q2, q3)
+    ph = calculate_p(h1, h2, h3)
+    p = pq/ph
+
+    print(f"p: {p}")
+
+    xp = np.linspace(x_start, x_end, 1000)
+    plt.figure(figsize=(10, 6))
+    plt.plot(xp, p, label=f"p", linestyle="dotted")
+    plt.xlabel("x")
+    plt.ylabel("h(x, t)")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(x1, h1, label=f"n = {n[0]}\nT = {T[0]}", linestyle="dotted")
+    plt.plot(x2, h2, label=f"n = {n[0]}\nT = {T[0]}", linestyle="dotted")
+    plt.plot(x3, h3, label=f"n = {n[0]}\nT = {T[0]}", linestyle="dotted")
+    plt.xlabel("x")
+    plt.ylabel("h(x, t)")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
 
 if __name__ == "__main__":
-    # first_task()
-    max_lambda = second_task()
+    first_task()
+    # max_lambda = second_task() #8.352979187303404
+    # third_task(max_lambda)
